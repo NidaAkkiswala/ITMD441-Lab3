4d1718067dc9b7295b7dfb8f2bf3aaaaffa65d7e
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2025-03-05 00:31:25 -0600

    exercise 1

--- nakkiswala-lab3.js
+++ nakkiswala-lab3.js
@@ -3,3 +3,22 @@
 
 // Exercise #1
 console.log("\nExercise #1 - minMaxAverage")
+function minMaxAverage(num) {
+    let totalNum = num.length
+    let minValue = Math.min(...num);
+    let maxValue = Math.max(...num);
+    let avg = num.reduce((sum, num) => sum + num, 0) / totalNum;
+    console.log(`Total Numbers: ${totalNum}, Min Value: ${minValue}, Max Value: ${maxValue}, Average: ${avg.toFixed(2)}`);
+
+}
+
+// Exercise #2
+console.log("\nExercise #2 - countVowels")
+
+
+// Exercise #3
+console.log("\nExercise #3 - sortNumbers")
+
+
+// Exercise #4
+console.log("\nExercise #4 - celsiusToFahrenheit")
