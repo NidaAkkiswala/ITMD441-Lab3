9354b5c573241955f7335fed252bcd42f00617cc
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2025-03-05 21:19:14 -0600

    exercise 4

--- nakkiswala-lab3.js
+++ nakkiswala-lab3.js
@@ -44,3 +44,16 @@ function sortNumbers(num) {
 sortNumbers([3, 4, 1, 8, 5]);
 sortNumbers([15, 3, 21, 11, 4]);
 sortNumbers([65, 23, 59, 10, 5, 25, 99, 66]);
+
+// Exercise #4
+console.log("\nExercise #4 - celsiusToFahrenheit")
+function celsiusToFahrenheit(cel) {
+    let celNum = parseFloat(cel);
+    let fahr = (celNum * 9/5) + 32;
+    console.log(`${celNum.toFixed(1)} Celsius = ${fahr.toFixed(1)} Fahrenheit`);
+}
+
+//Test Cases
+celsiusToFahrenheit(43);
+celsiusToFahrenheit(0);
+celsiusToFahrenheit(-3);
